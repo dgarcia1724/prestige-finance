eaeedfc7a25f8c5a6bd8e0f3dbc8643e5765041f
Danny Garcia <dgarcia1724@berkeley.edu>
2025-03-16 02:35:55 -0700

    Added top border to mobile bottom navbar

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -42,7 +42,7 @@ const MobileNavbar = () => {
   ];
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 h-16 bg-white shadow-lg md:hidden">
+    <nav className="fixed bottom-0 left-0 right-0 h-16 bg-white shadow-lg border-t border-gray-200 md:hidden">
       <div className="flex h-full items-center justify-around px-4">
         {navItems.map((item) => (
           <Link
