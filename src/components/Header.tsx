3bad02fbe98af3c62de3740ef433a7324c5c1598
Danny Garcia <dgarcia1724@berkeley.edu>
2025-03-16 02:06:35 -0700

    Added header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 right-0 h-16 bg-white shadow-sm z-10">
+      <div className="flex h-full items-center px-4">
+        <h1 className="text-xl font-semibold">Prestige</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
