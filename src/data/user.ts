9e737f144c96158d73963853bc6465be4f254951
Danny Garcia <dgarcia1724@berkeley.edu>
2025-03-17 23:52:55 -0700

    Can now add images from unsplash

--- src/data/user.ts
+++ src/data/user.ts
@@ -3,7 +3,8 @@ export const user = {
   name: "Madison Taylor",
   email: "madison.taylor@email.com",
   phone: "+1 555-9876",
-  profileImage: "/images/madison-profile.jpg",
+  profileImage:
+    "https://images.unsplash.com/photo-1494790108377-be9c29b29330?auto=format&fit=crop&w=500&h=500",
   joinedDate: "2023-08-21",
   address: "456 Park Ave, Los Angeles, CA",
 };
